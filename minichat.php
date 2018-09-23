8bba168ae17b915147da80d5b63f34fe993e4bcd
Anthony Labesse <labesseanthony63@gmail.com>
2018-09-23 10:36:19 +0200

    Création du formulaire

---
+++ minichat.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Mini-chat.com</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <h2>Mini-chat</h2>
+        
+        <p>
+        <form action="minichat_post.php" method="POST">
+            <p><label>Pseudonyme : <input type="text" name="prenom"/></label></p>
+            <p><label>Message : <input type ="textbox" name="message"/></label></p>
+            <p><input type="submit"/></p>
+        </form>
+        
+        <?php
+        /* Encore du PHP
+        Toujours du PHP */
+        ?>
+    </body>
+</html>
